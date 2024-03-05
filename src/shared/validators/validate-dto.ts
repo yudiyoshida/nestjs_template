73b8d75943d8d11fd77e2096c47afbb5936ac438
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-03-05 15:24:18 -0300

    fix: deletado arquivo não utilizado

--- src/shared/validators/validate-dto.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationError, validateSync } from 'class-validator';
-
-export function validateDto(dto: any): ValidationError[] {
-  return validateSync(dto);
-}
-
-export function getFieldErrors<T>(errors: ValidationError[], field: keyof T): ValidationError | undefined {
-  return errors.find(error => error.property === field);
-}
